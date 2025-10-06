f8261508ec386c8a6defd3167240978cb65605de
Ibrahim Yunus <ibrahim@kumonyall.co.uk>
2025-10-06 15:30:01 +0400

    Fix Stripe API version and disable TypeScript strict checking for build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    typedRoutes: true,
-  },
   images: {
     domains: ['maps.googleapis.com'],
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: true,
